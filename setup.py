cebd6e8f5bd8371bd4f8b7f7a26846a4e019cbe7
damien clochard <damien@dalibo.info>
2017-01-07 14:01:16 +0100

    modif CHANGELOG

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.7',
+    version='1.0.8',
 
     # The project's description
     description='A pandoc filter to insert barcodes and QR codes in a latex/pdf document',
